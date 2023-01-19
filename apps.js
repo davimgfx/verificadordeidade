69f3013aef1b91bc2dccceb8bf7133869dd07c3f
Davi Costa <118557337+davimgfx@users.noreply.github.com>
2023-01-19 15:04:07 -0300

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,46 @@
+function showAnswer(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert("ERRO! Verifique novamente!")   
+    } else{
+        var fsex = document.getElementsByName('radsexo')
+        var idade = ano - fano.value
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade <10){
+                img.setAttribute('src', 'fotos/foto-bebe-m.png')
+            }   
+            else if(idade < 21){
+                img.setAttribute('src', 'fotos/foto-jovem-m.png')
+            }
+            else if(idade < 50){
+                img.setAttribute('src', 'fotos/foto-adulto-m.png')
+            }
+            else{
+                img.setAttribute('src', 'fotos/foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+           if (idade >= 0 && idade <10){
+                img.setAttribute('src', 'fotos/foto-bebe-f.png')
+            }   
+            else if(idade < 21){
+                img.setAttribute('src', 'fotos/foto-jovem-f.png')
+            }
+            else if(idade < 50){
+                img.setAttribute('src', 'fotos/foto-adulto-f.png')
+            }
+            else{
+                img.setAttribute('src', 'fotos/foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
